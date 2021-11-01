1da7f55810c1f7faebcd43a0b4282e029a0d6799
Ammar Menda <43699506+AmmarMenda@users.noreply.github.com>
2021-11-01 14:10:17 +0530

    Update dwmstatus.sh

--- dwmstatus.sh
+++ dwmstatus.sh
@@ -2,7 +2,7 @@
 
 status () { 
 
-	echo -n "$(iwconfig wlp2s0| grep ESSID |awk '{print $4}')|BAT:$(acpi|awk '{print $3}')$(acpi|awk '{print $4}')$(acpi|awk '{print $5}')|$(date)"
+	echo -n "$(iwconfig (your network interface name)| grep ESSID |awk '{print $4}')|BAT:$(acpi|awk '{print $3}')$(acpi|awk '{print $4}')$(acpi|awk '{print $5}')|$(date)"
 }
 
 while :; do
