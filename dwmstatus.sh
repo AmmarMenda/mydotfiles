f0be53bb0a7c42a803f66f4d9116cd76bf211b6a
Ammar Menda <43699506+AmmarMenda@users.noreply.github.com>
2024-10-27 19:40:20 +0530

    Update dwmstatus.sh
    It's now more simpler no more unwanted things

--- dwmstatus.sh
+++ dwmstatus.sh
@@ -2,7 +2,7 @@
 
 status () { 
 
-	echo -n "$(iwconfig (your network interface name)| grep ESSID |awk '{print $4}')|BAT:$(acpi|awk '{print $3}')$(acpi|awk '{print $4}')$(acpi|awk '{print $5}')|$(date)"
+  echo -n "CPU:$(lscpu -e=CPU,MHZ |awk '{print $2}'|tail -n1)mHz|"TMP:$(sensors |awk '{print $2}'|tail -n 5|tail -n 2)"|BAT:$(acpi|awk '{print $3}')$(acpi|awk '{print $4}')|$(date +"%I:%M:%S %p")|$(xset -q |grep Cap|awk '{print $2 $3 $4}')|$(xset -q |grep Cap|awk '{print $6 $7 $8}')"
 }
 
 while :; do
